6c85d2ed67d80e37d4eadc48b8f98510ef180615
u20241222301@usco.edu.co <u20241222301@usco.edu.co>
2025-09-09 07:45:35 -0500

    creacion de paginas para mision, vision y equipo de trabajo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function mostrarenconsola(){
     console.log("su usuario es: "+usuario.value + " su contraseña es : "+ contraseña.value);
     }
+
+function alerta(){
+    alert("su nombre es: "+nombre.value);
+    }
+function cambiartamañodeletra(){
+ = ;
+}
+function cambiarcolorletra(){
+color = "pink";
+}
